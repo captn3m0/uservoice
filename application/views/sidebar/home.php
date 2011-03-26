ed5601331e8344f490fb1f851b51c9b78b15168e
Abhay Rana <capt.n3m0@gmail.com>
2011-03-26 14:25:04 +0530

    Created separate views for sidebar

---
+++ application/views/sidebar/home.php
@@ -0,0 +1,9 @@
+<div id="points-status">
+	  <div id="points-remaining">
+	    <strong>10</strong> votes left!
+	  </div>
+	<a class="points-help" href="#">What happens if I run out?</a>
+</div>
+<div class="ideas">
+	<h3>Your Ideas</h3>
+</div>
